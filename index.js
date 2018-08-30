f29cd836f77746014fd789ada5569ca0716984d2
jonathanemiranda <bloocheese002@Jonathans-MacBook-Pro.local>
2018-08-30 10:31:55 -0400

    added functionality to restart the game and change difficulty

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
-var colors = generateRandomColors(6);
-
+//VARIABLE DECLARITIONS
+var numberOfBoxes = 6;
+var colors = generateRandomColors(numberOfBoxes);
 var messageDisplay = document.querySelector("#message");
-
 var colorBoxes = document.querySelectorAll(".colorBox");
-
+var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easyButton");
+var hardButton = document.querySelector("#hardButton");
 var pickedColor = randomColor();
 var pickedColorDisplay = document.querySelector("#pickedColor");
 var nav = document.querySelector("nav");
 pickedColorDisplay.textContent = pickedColor;
 
+
+//NEW COLORS & PLAY AGAIN BUTTON
+resetButton.addEventListener("click", function(){
+	colors = generateRandomColors(numberOfBoxes);
+
+	pickedColor = randomColor();
+
+	pickedColorDisplay.textContent = pickedColor;
+
+	nav.style.backgroundColor = "#dddddd";
+
+	for(var i = 0; i < colorBoxes.length; i++){
+		colorBoxes[i].style.backgroundColor = colors[i];
+	}
+});
+
+
+//DIFFICULTY BUTTONS
+easyButton.addEventListener("click", function(){
+	numberOfBoxes = 3;
+	easyButton.classList.add("selected");
+	hardButton.classList.remove("selected");
+	colors = generateRandomColors(numberOfBoxes);
+	pickedColor = randomColor();
+	pickedColorDisplay.textContent = pickedColor;
+	for(var i = 0; i < colorBoxes.length; i++){
+		if(colors[i]){
+			colorBoxes[i].style.backgroundColor = colors[i];
+		}
+		else{
+			colorBoxes[i].style.display = "none";
+		}
+	}
+});
+
+hardButton.addEventListener("click", function(){
+	numberOfBoxes = 6;
+	hardButton.classList.add("selected");
+	easyButton.classList.remove("selected");
+	colors = generateRandomColors(numberOfBoxes);
+	pickedColor = randomColor();
+	pickedColorDisplay.textContent = pickedColor;
+	for(var i = 0; i < colorBoxes.length; i++){
+		if(colors[i]){
+			colorBoxes[i].style.backgroundColor = colors[i];
+			colorBoxes[i].style.display = "block";
+	}
+}});
+
+
+//
 for (var i = 0; i < colorBoxes.length; i++) {
 	//add initial colors
 	colorBoxes[i].style.backgroundColor = colors[i];
@@ -21,7 +74,8 @@ for (var i = 0; i < colorBoxes.length; i++) {
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct!";
 			changeColors(pickedColor);
-			nav.style.backgroundColor = pickedColor;
+			nav.style.backgroundColor = pickedColor
+			resetButton.textContent = "Play Again";
 
 		}
 		else{
