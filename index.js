d3e86d8074a2e5d6902eef5ce426f508f9eccf0f
jonathanemiranda <jonathanmiranda93@gmail.com>
2018-09-05 13:42:47 -0600

    finished refactor

--- index.js
+++ index.js
@@ -1,14 +1,64 @@
 //VARIABLE DECLARITIONS
 var numberOfBoxes = 6;
-var colors = generateRandomColors(numberOfBoxes);
+var colors = [];
+var pickedColor;
 var messageDisplay = document.querySelector("#message");
 var colorBoxes = document.querySelectorAll(".colorBox");
 var resetButton = document.querySelector("#reset");
 var difficultyButtons = document.querySelectorAll(".difficultyButton");
-var pickedColor = randomColor();
 var pickedColorDisplay = document.querySelector("#pickedColor");
 var nav = document.querySelector("nav");
-pickedColorDisplay.textContent = pickedColor;
+
+
+
+//INIT
+init();
+
+function init(){
+	modeButtonListeners();
+	setUpBoxes();
+	reset();
+}
+
+//DIFFICULTY BUTTONS
+	function modeButtonListeners(){
+		for (var i = 0; i < difficultyButtons.length; i++) {
+			difficultyButtons[i].addEventListener("click", function(){
+			difficultyButtons[0].classList.remove("selected");
+			difficultyButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			this.textContent == "Easy" ? numberOfBoxes = 3 : numberOfBoxes = 6;
+			reset();
+
+			});
+		}
+	}
+
+
+//GAMEPLAY LOGIC
+	function setUpBoxes(){
+		for (var i = 0; i < colorBoxes.length; i++) {
+
+		//add click listeners
+		colorBoxes[i].addEventListener("click", function(){
+			//grab color of clicked sqaure
+			var clickedColor = this.style.backgroundColor;
+			//compare to pickedColor
+			if(clickedColor === pickedColor){
+				messageDisplay.textContent = "Correct!";
+				changeColors(pickedColor);
+				nav.style.backgroundColor = pickedColor
+				resetButton.textContent = "Play Again";
+
+			}
+			else{
+				this.style.backgroundColor = "#232323";
+				messageDisplay.textContent = "Try Again";
+			}
+			});
+
+		}
+	}
 
 
 //NEW COLORS & PLAY AGAIN BUTTON
@@ -16,18 +66,6 @@ resetButton.addEventListener("click", function(){
 	reset();
 });
 
-
-//DIFFICULTY BUTTONS
-for (var i = 0; i < difficultyButtons.length; i++) {
-	difficultyButtons[i].addEventListener("click", function(){
-		difficultyButtons[0].classList.remove("selected");
-		difficultyButtons[1].classList.remove("selected");
-		this.classList.add("selected");
-		this.textContent == "Easy" ? numberOfBoxes = 3 : numberOfBoxes = 6;
-		reset();
-
-	});
-}
 
 
 //RESET
@@ -56,30 +94,7 @@ function reset(){
 }
 
 
-//GAMEPLAY LOGIC
-for (var i = 0; i < colorBoxes.length; i++) {
-	//add initial colors
-	colorBoxes[i].style.backgroundColor = colors[i];
 
-	//add click listeners
-	colorBoxes[i].addEventListener("click", function(){
-		//grab color of clicked sqaure
-		var clickedColor = this.style.backgroundColor;
-		//compare to pickedColor
-		if(clickedColor === pickedColor){
-			messageDisplay.textContent = "Correct!";
-			changeColors(pickedColor);
-			nav.style.backgroundColor = pickedColor
-			resetButton.textContent = "Play Again";
-
-		}
-		else{
-			this.style.backgroundColor = "#232323";
-			messageDisplay.textContent = "Try Again";
-		}
-	});
-
-}
 
 
 function changeColors(color){
